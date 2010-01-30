e713e8b36400d39fa37244d9fc9479dc6317094f
Nick Fisher <github@nickdafish.com>
2010-01-30 18:36:46 -0500

    Moving stuff around

--- redbrake.rb
+++ redbrake.rb
@@ -3,7 +3,34 @@
 require 'yaml'
 
 module RedBrake
-  def self.scan(input_path, quiet = false)
+  class Source < Struct.new :path
+    def initilize
+      raw = RedBrake.scan :path
+      self.titles = {}
+      raw.each do |title_number, title_data|
+        self.titles['title_number'] = Title.new title_number,
+                                              Time.new(title_data['duration'])
+        title_data['chapters'].each do |chapter_number, chapter_data|
+          self.titles['title_number'].chapters[chapter_number] = \
+                                                  chapter_data['duration']
+        end
+      end
+    end
+  end
+  class Title < Struct.new :number, :duration
+    def initilize
+      
+    end
+  end
+  class Chapter < Struct.new :number, :duration
+  end
+
+  def self.scan input_path
+    output = self.read_source input_path
+    self.output_to_hashes output
+  end
+
+  def self.read_source(input_path, quiet = false)
 
     unless quiet
       print 'Reading input... '
@@ -14,39 +41,51 @@ module RedBrake
       puts 'DONE'
       STDOUT.flush
     end
+    output
+
+  end
+
+  def self.restructure output
+    # FIXME - quote title duration!
+    # FIXME - extract chapter duration!
 
     # Remove random output
-    output.gsub! /^(?![ ]*\+ ).*\n/, ''
+    output.gsub!(/^(?![ ]*\+ ).*\n/, '')
 
     # Strip the '+ ' prefix off each line without loosing indent
-    output.gsub! /^([ ]*)\+ (\w)/, '\1\2'
+    output.gsub!(/^([ ]*)\+ (\w)/, '\1\2')
 
     # Make number of angles a property
-    output.gsub! /^  angle\(s\) (\d+)$/, '  angle(s): \1'
+    output.gsub!(/^  angle\(s\) (\d+)$/, '  angle(s): \1')
 
     # Make track listings to dicts
-    output.gsub! /^([ ]*)(\d+), (\w+ .*)/, '\1\2: \'\3\''
+    output.gsub!(/^([ ]*)(\d+), (\w+ .*)/, '\1\2: \'\3\'')
 
     # Quote all chapter dictionary values
-    output.gsub! /^(    \d+: )(cells \d.*)/, '\1\'\2\''
+    output.gsub!(/^(    \d+: )(cells \d.*)/, '\1\'\2\'')
 
     # Make interlace detection in ot a property
-    output.gsub! /combing detected.*/, 'interlaced: true'
+    output.gsub!(/combing detected.*/, 'interlaced: true')
 
     # Split up that first line of stuff
-    output.gsub! /vts (\d+), ttn (\d+), cells (.*)/,
-                    "vts: \\1\n  ttn: \\2\n  cells: \\3"
+    output.gsub!(/vts (\d+), ttn (\d+), (cells) (.*)/,
+                    "vts: \\1\n  ttn: \\2\n  \\3: \\4")
 
     # Split up another long line
-    output.gsub! /, (pixel aspect: )(.*?), (display aspect: )(.*?), (.*?) (fps)/,
-                    "\n  \\1'\\2'\n  \\3\\4\n  \\6: \\5"
+    output.gsub!(/, (pixel aspect: )(.*?),/, "\n  \\1'\\2'\n")
+    output.gsub!(/ (display aspect: )(.*?),/, "  \\1\\2\n")
+    output.gsub!(/ (.*?) (fps)/, "  \\2: \\1")
 
     # Quote autocrop value
-    output.gsub! /(autocrop: )(.*)/, '\1\'\2\''
+    output.gsub!(/(autocrop: )(.*)/, '\1\'\2\'')
 
     # Remake the title as a straight number
-    output.gsub! /^title (\d+:)$/, '\1'
+    output.gsub!(/^title (\d+:)$/, '\1')
+    output
+  end
 
+  def self.output_to_hashes output
+    output = self.restructure output
     YAML::load(output)
   end
 end
