b17542654993e943415617d1dc1531c6e9bf9907
Nick Fisher <github@nickdafish.com>
2010-03-07 14:09:56 -0500

    Better interlace testing

--- spec/test.rb
+++ spec/test.rb
@@ -1,9 +1,42 @@
 require 'redbrake'
 SAMPLEFILE_PATH = 'tests/sample_scan.txt'
 
+class NegativeRegex
+  def initialize(regex)
+    @regex = regex
+  end
+  def description
+    "string is not supposed to match #{@regex}"
+  end
+  def ==(string)
+    string =~ @regex ? false : true
+  end
+end
+
+def negative_re(regex)
+  NegativeRegex.new(regex)
+end
+
 describe RedBrake::Source do
-  before(:all){@src = RedBrake::Source.new(File.new(SAMPLEFILE_PATH).read)}
+  before(:all) do
+    RedBrake::LOG.level = 999
+    @src = RedBrake::Source.new(File.new(SAMPLEFILE_PATH).read)
+  end
   it('Should have 9 titles'){@src.should have(9).titles}
+  describe 'Title 7' do
+    before(:each){@title = @src.titles[7]}
+    it('Should have 9 chapters'){@title.should have(9).chapters}
+    it('Should not be interlaced'){@title.interlaced.should be_false}
+    it 'Should not pass deinterlace to encode' do
+      @title.should_receive(:base_encode).with(
+                                      hash_not_including(:deinterlace => true))
+      @title.encode
+    end
+    it 'Should encode without DEINTERLACE' do
+      @title.should_receive(:run_cmd).with(negative_re(/.*-d /))
+      @title.encode
+    end
+  end
   describe 'Title 9' do
     before(:each){@title = @src.titles[9]}
     it('Should have 2 chapters'){@title.should have(2).chapters}
